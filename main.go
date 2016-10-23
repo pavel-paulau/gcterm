07f6d8a6805afe6a2dfb9fd563b13794ed36fb9a
Pavel Paulau <pavel.paulau@gmail.com>
2016-10-23 15:39:37 -0700

    Move UI components to a separate file

--- main.go
+++ main.go
@@ -30,50 +30,6 @@ func init() {
 	height = (height - barHeight) / 2
 }
 
-func newPar(label string) *termui.Par {
-	par := termui.NewPar("")
-
-	par.BorderLabel = label
-	par.BorderFg = termui.ColorWhite
-	par.BorderLabelFg = termui.ColorWhite
-	par.Height = barHeight
-	par.PaddingLeft = 10
-
-	return par
-}
-
-func newGauge(label string) *termui.Gauge {
-	gauge := termui.NewGauge()
-
-	gauge.BarColor = termui.ColorRed
-	gauge.Border = true
-	gauge.BorderLabel = label
-	gauge.BorderLabelFg = termui.ColorWhite
-	gauge.Height = barHeight
-	gauge.LabelAlign = termui.AlignCenter
-	gauge.PaddingLeft = 1
-	gauge.PaddingRight = 1
-	gauge.Percent = 0
-	gauge.PercentColorHighlighted = termui.AttrBold
-
-	return gauge
-}
-
-func newLineChart(label string) *termui.LineChart {
-	lineChart := termui.NewLineChart()
-
-	lineChart.AxesColor = termui.ColorWhite
-	lineChart.BorderLabel = label
-	lineChart.BorderLabelFg = termui.ColorWhite
-	lineChart.Data = make([]float64, width)
-	lineChart.DataLabels = make([]string, width)
-	lineChart.Height = height
-	lineChart.LineColor = termui.ColorGreen | termui.AttrBold
-	lineChart.Mode = "dot"
-
-	return lineChart
-}
-
 func addTimeLabels(lc *termui.LineChart) {
 	t := time.Now()
 
